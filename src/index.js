991e2fb90f4e05d1b9df86d1ec301baf66c63ded
Frank J. Reashore <reashore@telus.net>
2017-11-05 09:00:42 -0800

    Changed to old router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route} from 'react-router';
-//import {Router, Route, IndexRoute} from 'react-router';
+//import ReactRouter from 'react-router';
+//import {Router, Route} from 'react-router';
+import ReactRouter from 'react-router';
 import registerServiceWorker from './registerServiceWorker';
 // eslint-disable-next-line
 import Home from './Components/Home';
+var {Router, Route, IndexRoute, IndexLink, Link} = ReactRouter;
 
 ReactDOM.render(
     <Router>
         <Route path="/" component="{App}">
-            {/* <IndexRoute component={Home}/> */}
+            <IndexRoute component={Home}/>
             <Route path="Home" component="{Home}"/>
             <Route path="about" component="{About}"/>
             <Route path="contact" component="{Contact}"/>
